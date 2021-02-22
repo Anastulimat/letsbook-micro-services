31524e9d6dfbbc5e905f5c28942ccfa3756e26b0
Anas TULIMAT <anas.tulimat@outlook.fr>
2021-02-22 19:22:52 +0100

    fix: add database url in .env add cors to jwt auth backend

--- docker-jwt-user-auth/app.js
+++ docker-jwt-user-auth/app.js
@@ -16,6 +16,14 @@ require("./db");
 
 const ROOT = __dirname + '/';
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
 app.get('/api', (req, res) => {
     res.status(200).send('API works.');
 });
